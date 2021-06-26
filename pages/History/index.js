6e5bad2ae9356c761cf1438697c63b1eef2610ae
Michael Sun <i@cctv3.net>
2021-06-26 15:52:04 +0800

    🐜  优化了历史页面的时间的生成的规则，表达更加真实

--- pages/History/index.js
+++ pages/History/index.js
@@ -1,6 +1,15 @@
 import moment from 'moment';
 import React, {useState, useEffect} from 'react';
-import {AppRegistry, View, StyleSheet, Text, SafeAreaView} from 'react-native';
+import {
+  AppRegistry,
+  View,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
 import TabBar from '../../components/TabBar';
 import Tabs from '../../components/Tabs';
 import List from './List';
@@ -9,15 +18,31 @@ const History = props => {
   const [datas, setDatas] = useState([]);
 
   const createDatas = () => {
-    return Array.from({length: 10}, (_, i) => [
-      moment().format('YYYY-MM-DD HH:mm:ss'),
-      moment().format('YYYY-MM-DD HH:mm:ss'),
-      '深圳XX医检验实验室'.replace(
-        'XX',
-        ['金域', '海普洛斯'][parseInt(Math.random() * 2)],
-      ),
-      '阴性',
-    ]);
+    let days = [];
+    let _datas = [];
+    for (let i = 0; i < 10; i++) {
+      days.push(parseInt(Math.random() * 100));
+    }
+    days = days.sort((a, b) => a - b);
+    for (let i = 0; i < days.length; i++) {
+      _datas.push([
+        moment()
+          .add('days', -1 * days[i])
+          .format('YYYY-MM-DD HH:mm:ss'),
+        moment()
+          .add('days', -1 * days[i])
+          .add('hours', parseInt(Math.random() * 24))
+          .add('minutes', parseInt(Math.random() * 60))
+          .add('seconds', parseInt(Math.random() * 60))
+          .format('YYYY-MM-DD HH:mm:ss'),
+        '深圳XX医检验实验室'.replace(
+          'XX',
+          ['金域', '海普洛斯'][parseInt(Math.random() * 2)],
+        ),
+        '阴性',
+      ]);
+    }
+    return _datas;
   };
 
   useEffect(() => {
@@ -39,7 +64,17 @@ const History = props => {
           title="核酸检测记录"
         />
         <View style={{height: 16}} />
-        <Text style={styles.textTitle}>核酸检测记录</Text>
+        <View style={styles.viewHeader}>
+          <Text style={styles.textTitle}>核酸检测记录</Text>
+          <TouchableOpacity style={styles.viewRefreshAll} onPress={() => {}}>
+            <Image
+              source={require('../../images/history_refresh.png')}
+              style={styles.imageRefresh}
+            />
+            <View style={{width: 4}} />
+            <Text style={styles.textRefresh}>刷新</Text>
+          </TouchableOpacity>
+        </View>
         <View style={{height: 24}} />
         <Tabs
           tabs={['检测完成', '检测中']}
@@ -66,6 +101,26 @@ const styles = StyleSheet.create({
     fontSize: 36,
     color: 'black',
     fontWeight: '500',
+  },
+  viewHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    display: 'flex',
+  },
+  viewRefreshAll: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    display: 'flex',
+  },
+  imageRefresh: {
+    height: 20,
+    width: 20,
+    tintColor: '#8bdc61',
+  },
+  textRefresh: {
+    fontSize: 18,
+    color: 'black',
   },
 });
 
